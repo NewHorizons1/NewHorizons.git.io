c514a1cf588cb389a3884b9fa022d2fe50c27127
Angelo <87893288+NewHorizons1@users.noreply.github.com>
2021-09-19 16:20:14 +0900

    Update gserver-logger.rb

--- gserver-logger.rb
+++ gserver-logger.rb
@@ -27,3 +27,21 @@ private
       log.gets                        # ignore partial line
       log.read                        # and return rest
     end
+  def get_end_of_log_file
+    File.open("/var/log/system.log") do |log|
+      log.seek(-500, IO::SEEK_END)    # back up 500 characters from end
+      log.gets                        # ignore partial line
+      log.read                        # and return rest
+    end
+  def get_end_of_log_file
+    File.open("/var/log/system.log") do |log|
+      log.seek(-500, IO::SEEK_END)    # back up 500 characters from end
+      log.gets                        # ignore partial line
+      log.read                        # and return rest
+    end
+  def get_end_of_log_file
+    File.open("/var/log/system.log") do |log|
+      log.seek(-500, IO::SEEK_END)    # back up 500 characters from end
+      log.gets                        # ignore partial line
+      log.read                        # and return rest
+    end
